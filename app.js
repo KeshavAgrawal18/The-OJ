b822d6ead8dcd40703dc3ee5d88fd8455266ffb0
Keshav Agrawal <Keshavagrawal9471408042@gmail.com>
2023-10-31 07:19:04 +0530

    add env file and connect mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const express = require("express")
-const PORT = 3000;
+require('dotenv').config()
+const express = require("express");
+const PORT = process.env.PORT;
 const app = express();
+const bodyParser =require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(express.static(__dirname + "/public"))
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  const db = mongoose.connection;
+  
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", function () {
+    console.log("Connected to MongoDB...");
+  });  
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -12,4 +28,4 @@ app.get("/", (req, res) => {
 app.listen(PORT, (req, res) => {
     console.log("App is listening on port: " + PORT);
     console.log("http://localhost:" + PORT);
-});
+}); 
